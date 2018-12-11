12f1e77e089d8a4c7d0f1ef486d10838c1939832
面﨑祐一 <Uone@Omozaki-no-MacBook-Air.local>
2018-12-11 17:15:35 +0900

    12/11 17:15

--- eclipse_workspace/src/Sketch.java
+++ eclipse_workspace/src/Sketch.java
@@ -3,23 +3,27 @@ import processing.core.PApplet;
 public class Sketch extends PApplet{
 	int size = 700;
 
+	double[][] x;
+
 	public void settings() {
 		size(size, size);
 	}
 
 	public void setup() {
-		String path = "";
+		String path = "src/input/5_1/dtlz1Rand_4obj.dat";
+		read(path, ", ");
+		System.out.println(x[0].length);
 	}
 
-	public void read(String _path, String _separator, double[][] _x) {
+	public void read(String _path, String _separator) {
 		String[] line = loadStrings(_path);
 		int n_solution = line.length;
 		int n_objective = line[0].split(_separator).length;
 
-		_x = new double[n_solution][n_objective];
+		this.x = new double[n_solution][n_objective];
 		for(int i = 0; i < n_solution; i++) {
 			for(int j = 0; j < n_objective; j++) {
-				_x[i][j] = Double.parseDouble(line[i].split(_separator)[j]);
+				this.x[i][j] = Double.parseDouble(line[i].split(_separator)[j]);
 			}
 		}
 	}
